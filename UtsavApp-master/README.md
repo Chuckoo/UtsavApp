8df1adcbc684cf0c643c11eec0ef6f9138e43b08
Rahul R S <40643724+rrsrahul@users.noreply.github.com>
2020-02-29 20:18:00 +0530

    Add files via upload

---
+++ UtsavApp-master/README.md
@@ -0,0 +1,30 @@
+# Flutter Quick UI  -  Watch Shop
+
+A Flutter UI implementation of a Watch Shop App inspired by <a href="https://dribbble.com/hafizrana">Hafiz Rana's</a> desgin on <a href="https://dribbble.com/shots/8088100-Mobile-app-Watch-Shop">Dribble</a>.
+
+
+Star this repo if you like what you see.
+
+## 📸 Screenshots
+
+<img src="screenshots/banner.png"/>
+
+<img src="screenshots/1.png" width="250"/> <img src="screenshots/2.png" width="250"/> <img src="screenshots/3.png" width="250"/>
+
+
+## Author(s)
+**Emmanuel Fache**
+
+## Getting Started
+
+**Note**: Make sure your Flutter environment is setup.
+#### Installation
+
+In the command terminal, run the following commands:
+
+    $ git clone https://github.com/emrade/flutter-watch-shop-ui.git flutter_watch_shop
+    $ cd flutter_watch_shop/
+    $ flutter packages get
+    $ flutter run
+
+##### Check out Flutter’s online [documentation](http://flutter.io/) for help getting started with your Flutter project.
---
+++ UtsavApp-master/lib/global_widgets/timer.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> with TickerProviderStateMixin {
+  AnimationController controller;
+
+  // bool isPlaying = false;
+
+  String get timerString {
+    Duration duration = controller.duration * controller.value;
+    return '${duration.inDays}D ${(duration.inHours % 24).toString().padLeft(2, '0')}H';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(days:70,hours: 0),
+    );
+
+    // ..addStatusListener((status) {
+    //     if (controller.status == AnimationStatus.dismissed) {
+    //       setState(() => isPlaying = false);
+    //     }
+
+    //     print(status);
+    //   })
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.center,
+                child: AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned.fill(
+                        child: AnimatedBuilder(
+                          animation: controller,
+                          builder: (BuildContext context, Widget child) {
+                            return CustomPaint(
+                                painter: TimerPainter(
+                                  animation: controller,
+                                  backgroundColor: Colors.white,
+                                  color: themeData.indicatorColor,
+                                ));
+                          },
+                        ),
+                      ),
+                      Align(
+                        alignment: FractionalOffset.center,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Utsav",
+                              style: TextStyle(fontSize: 50,color: Colors.red,fontStyle: FontStyle.italic,fontFamily: 'Times New Roman')
+                            ),
+                            AnimatedBuilder(
+                                animation: controller,
+                                builder: (BuildContext context, Widget child) {
+                                  return Text(
+                                    timerString,
+                                    style: themeData.textTheme.display1,
+                                  );
+                                }),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  FloatingActionButton(
+                    child: AnimatedBuilder(
+                      animation: controller,
+                      builder: (BuildContext context, Widget child) {
+                        return Icon(controller.isAnimating
+                            ? Icons.pause
+                            : Icons.play_arrow);
+
+                        // Icon(isPlaying
+                        // ? Icons.pause
+                        // : Icons.play_arrow);
+                      },
+                    ),
+                    onPressed: () {
+                      // setState(() => isPlaying = !isPlaying);
+
+                      if (controller.isAnimating) {
+                        controller.stop(canceled: true);
+                      } else {
+                        controller.reverse(
+                            from: controller.value == 0.0
+                                ? 1.0
+                                : controller.value);
+                      }
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TimerPainter extends CustomPainter {
+  TimerPainter({
+    this.animation,
+    this.backgroundColor,
+    this.color,
+  }) : super(repaint: animation);
+
+  final Animation<double> animation;
+  final Color backgroundColor, color;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = backgroundColor
+      ..strokeWidth = 5.0
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+
+    //canvas.drawCircle(size.center(Offset.zero), size.width / 2.0, paint);
+    paint.color = color;
+    //canvas.drawArc(Offset.zero & size, math.pi * 1.5, -progress, false, paint);
+  }
+
+  @override
+  bool shouldRepaint(TimerPainter old) {
+    return animation.value != old.animation.value ||
+        color != old.color ||
+        backgroundColor != old.backgroundColor;
+  }
+}
